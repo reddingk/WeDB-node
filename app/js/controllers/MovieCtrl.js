c56b90a8c0c9363d1289894efdfc010c30282cd5
Kris Redding <kredding@inovas.net>
2016-05-03 16:45:28 -0400

    Update movie select styles

--- app/js/controllers/MovieCtrl.js
+++ app/js/controllers/MovieCtrl.js
@@ -1,10 +1,79 @@
 (function(){
    "use strict";
 
-    angular.module('movieCtrl', []).controller('MovieController', ['movieServices','MovieData', function(movieServices, MovieData){
+    angular.module('movieCtrl', ['ui.bootstrap']).controller('MovieController', ['movieServices','MovieData', function(movieServices, MovieData){
       var vm = this;
       vm.title = "Movie";
-      
+
+      vm.resultsLimit = 8;
+      vm.selected = undefined;
+      vm.selectedMovie = undefined;
+      vm.showSelected = false;
+      vm.selectedCredit = undefined;
+      vm.showSelectedCredit = false;
+
+
+      vm.movieSearch = function(query) {
+        return movieServices.names(query).then(function (results) {
+          var moviedata = results.results;
+          return (moviedata.length > vm.resultsLimit ? moviedata.slice(0, vm.resultsLimit) : moviedata);  ;
+        }, function (error) {
+          console.log("ERROR NO RESULTS");
+        });
+      }
+
+      vm.getMovieInfo = function(movieid) {
+        return movieServices.info(movieid).then(function (results) {
+          return results;
+        }, function (error) {
+          console.log("ERROR NO MOVIE INFO");
+        });
+      }
+
+      vm.getMovieCredits = function(movieid) {
+        return movieServices.credits(movieid).then(function (results) {
+          return results;
+        }, function (error) {
+          console.log("ERROR NO MOVIE CREDITS");
+        });
+      }
+
+      vm.viewMovieInfo = function(item) {
+        vm.showSelected = false;
+        var movieid = item.id;
+        if((vm.selectedMovie == undefined) || (movieid != vm.selectedMovie.id)) {
+          vm.showSelectedCredit = false;
+          vm.selectedCredit = undefined;
+          vm.getMovieInfo(movieid).then(function(retResults) {
+            vm.showSelected = true;
+            vm.selectedMovie = retResults;
+          });
+        }
+        else {
+          vm.showSelected = true;
+        }
+      }
+
+      vm.viewMovieCredits = function(movieid) {
+        if(vm.showSelectedCredit == false){
+          if(vm.selectedCredit == undefined) {
+            vm.getMovieCredits(movieid).then(function(retResults) {
+              vm.selectedCredit = retResults.cast;
+              vm.showSelectedCredit = true;
+              console.log("Got Results");
+            });
+          }
+          else {
+            vm.showSelectedCredit = true;
+            console.log("Already Had Results");
+          }
+        }
+        else {
+          vm.showSelectedCredit = false;
+        }
+
+      }
+
     }]);
 
 })();
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6,168 +6,6 @@
 })();
 
 (function(){
-   "use strict";
-
-    angular.module('castCtrl', []).controller('CastController', function($scope){
-      var vm = this;
-      vm.title = "Cast";
-
-    });
-
-})();
-
-(function(){
-   "use strict";
-
-    angular.module('homeCtrl', []).controller('HomeController', function($scope){
-      var vm = this;
-      vm.title = "Home";
-      vm.sections = [
-           //{order:'0', title:'Special', controller:'SpecialController', type:'special'}
-           {order:'3', title:'Actors/Actresses', controller:'CastController', state:'cast', type:'cast-tab'},
-           {order:'1', title:'Movie', controller:'MovieController', state:'movies', type:'movie-tab' },
-           {order:'2', title:'Tv Shows', controller:'TvController', state:'tv', type:'tv-tab' }
-       ];
-    });
-
-})();
-
-(function(){
-   "use strict";
-
-    angular.module('movieCtrl', []).controller('MovieController', ['movieServices','MovieData', function(movieServices, MovieData){
-      var vm = this;
-      vm.title = "Movie";
-      
-    }]);
-
-})();
-
-(function(){
-   "use strict";
-
-    angular.module('specialCtrl', ['ui.bootstrap']).controller('SpecialController', [ 'movieServices','MovieData', function(movieServices, MovieData){
-      var vm = this;
-      vm.title = "Special";
-      vm.tstmsg = "waiting";
-
-
-      vm.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana'];
-      vm.statesWithFlags = [{'name':'Alabama','flag':'5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png'},{'name':'Alaska','flag':'e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png'},{'name':'Arizona','flag':'9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png'},{'name':'Arkansas','flag':'9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png'},{'name':'California','flag':'0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png'},{'name':'Colorado','flag':'4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png'},{'name':'Connecticut','flag':'9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png'},{'name':'Delaware','flag':'c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png'},{'name':'Florida','flag':'f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png'},{'name':'Georgia','flag':'5/54/Flag_of_Georgia_%28U.S._state%29.svg/46px-Flag_of_Georgia_%28U.S._state%29.svg.png'},{'name':'Hawaii','flag':'e/ef/Flag_of_Hawaii.svg/46px-Flag_of_Hawaii.svg.png'},{'name':'Idaho','flag':'a/a4/Flag_of_Idaho.svg/38px-Flag_of_Idaho.svg.png'},{'name':'Illinois','flag':'0/01/Flag_of_Illinois.svg/46px-Flag_of_Illinois.svg.png'},{'name':'Indiana','flag':'a/ac/Flag_of_Indiana.svg/45px-Flag_of_Indiana.svg.png'},{'name':'Iowa','flag':'a/aa/Flag_of_Iowa.svg/44px-Flag_of_Iowa.svg.png'},{'name':'Kansas','flag':'d/da/Flag_of_Kansas.svg/46px-Flag_of_Kansas.svg.png'},{'name':'Kentucky','flag':'8/8d/Flag_of_Kentucky.svg/46px-Flag_of_Kentucky.svg.png'},{'name':'Louisiana','flag':'e/e0/Flag_of_Louisiana.svg/46px-Flag_of_Louisiana.svg.png'},{'name':'Maine','flag':'3/35/Flag_of_Maine.svg/45px-Flag_of_Maine.svg.png'},{'name':'Maryland','flag':'a/a0/Flag_of_Maryland.svg/45px-Flag_of_Maryland.svg.png'},{'name':'Massachusetts','flag':'f/f2/Flag_of_Massachusetts.svg/46px-Flag_of_Massachusetts.svg.png'},{'name':'Michigan','flag':'b/b5/Flag_of_Michigan.svg/45px-Flag_of_Michigan.svg.png'},{'name':'Minnesota','flag':'b/b9/Flag_of_Minnesota.svg/46px-Flag_of_Minnesota.svg.png'},{'name':'Mississippi','flag':'4/42/Flag_of_Mississippi.svg/45px-Flag_of_Mississippi.svg.png'},{'name':'Missouri','flag':'5/5a/Flag_of_Missouri.svg/46px-Flag_of_Missouri.svg.png'},{'name':'Montana','flag':'c/cb/Flag_of_Montana.svg/45px-Flag_of_Montana.svg.png'},{'name':'Nebraska','flag':'4/4d/Flag_of_Nebraska.svg/46px-Flag_of_Nebraska.svg.png'},{'name':'Nevada','flag':'f/f1/Flag_of_Nevada.svg/45px-Flag_of_Nevada.svg.png'},{'name':'New Hampshire','flag':'2/28/Flag_of_New_Hampshire.svg/45px-Flag_of_New_Hampshire.svg.png'},{'name':'New Jersey','flag':'9/92/Flag_of_New_Jersey.svg/45px-Flag_of_New_Jersey.svg.png'},{'name':'New Mexico','flag':'c/c3/Flag_of_New_Mexico.svg/45px-Flag_of_New_Mexico.svg.png'},{'name':'New York','flag':'1/1a/Flag_of_New_York.svg/46px-Flag_of_New_York.svg.png'},{'name':'North Carolina','flag':'b/bb/Flag_of_North_Carolina.svg/45px-Flag_of_North_Carolina.svg.png'},{'name':'North Dakota','flag':'e/ee/Flag_of_North_Dakota.svg/38px-Flag_of_North_Dakota.svg.png'},{'name':'Ohio','flag':'4/4c/Flag_of_Ohio.svg/46px-Flag_of_Ohio.svg.png'},{'name':'Oklahoma','flag':'6/6e/Flag_of_Oklahoma.svg/45px-Flag_of_Oklahoma.svg.png'},{'name':'Oregon','flag':'b/b9/Flag_of_Oregon.svg/46px-Flag_of_Oregon.svg.png'},{'name':'Pennsylvania','flag':'f/f7/Flag_of_Pennsylvania.svg/45px-Flag_of_Pennsylvania.svg.png'},{'name':'Rhode Island','flag':'f/f3/Flag_of_Rhode_Island.svg/32px-Flag_of_Rhode_Island.svg.png'},{'name':'South Carolina','flag':'6/69/Flag_of_South_Carolina.svg/45px-Flag_of_South_Carolina.svg.png'},{'name':'South Dakota','flag':'1/1a/Flag_of_South_Dakota.svg/46px-Flag_of_South_Dakota.svg.png'},{'name':'Tennessee','flag':'9/9e/Flag_of_Tennessee.svg/46px-Flag_of_Tennessee.svg.png'},{'name':'Texas','flag':'f/f7/Flag_of_Texas.svg/45px-Flag_of_Texas.svg.png'},{'name':'Utah','flag':'f/f6/Flag_of_Utah.svg/45px-Flag_of_Utah.svg.png'},{'name':'Vermont','flag':'4/49/Flag_of_Vermont.svg/46px-Flag_of_Vermont.svg.png'},{'name':'Virginia','flag':'4/47/Flag_of_Virginia.svg/44px-Flag_of_Virginia.svg.png'},{'name':'Washington','flag':'5/54/Flag_of_Washington.svg/46px-Flag_of_Washington.svg.png'},{'name':'West Virginia','flag':'2/22/Flag_of_West_Virginia.svg/46px-Flag_of_West_Virginia.svg.png'},{'name':'Wisconsin','flag':'2/22/Flag_of_Wisconsin.svg/45px-Flag_of_Wisconsin.svg.png'},{'name':'Wyoming','flag':'b/bc/Flag_of_Wyoming.svg/43px-Flag_of_Wyoming.svg.png'}];
-
-      vm.selected = undefined;
-
-      vm.movieTest = function() {
-
-        movieServices.names("Best Man").then(function (results) {
-          vm.results = results;
-          console.log("Finished Get");
-          vm.tstmsg = "Got results"
-        }, function (error) {
-          vm.tstmsg = "ERROR: Nothing";
-        });
-      }
-      vm.movieSearch = function(query) {
-        console.log("Search: " + query);
-
-        movieServices.names(query).then(function (results) {
-          console.log(results.results);
-          return results.results.map(function(item){
-            return item.title;
-          });
-        }, function (error) {
-          console.log("ERROR NO RESULTS");
-        });
-      }
-
-    }]);
-
-})();
-
-(function(){
-   "use strict";
-
-    angular.module('tvCtrl', []).controller('TvController', function($scope){
-      var vm = this;
-      vm.title = "Tv";
-
-    });
-
-})();
-
-(function(){
-   "use strict";
-
-   angular.module('services', [])
-    .service('movieServices', ['$q', '$http','api', function MovieService($q, $http, api) {
-      return {
-        names: function($str){
-          var def = $q.defer();
-          $http({
-            method: 'GET',
-            url: api.movie.searchname($str)
-          }).then(function successCallback(response) {
-            def.resolve(response.data);
-          }, function errorCallback(response) {
-            def.reject(response);
-          });
-          return def.promise;
-        },
-        credits: function($mid){
-          var def = $q.defer();
-          $http({
-            method: 'GET',
-            url: api.movie.getMovieCredits($mid)
-          }).then(function successCallback(response) {
-            def.resolve(response.data);
-          }, function errorCallback(response) {
-            def.reject(response);
-          });
-          return def.promise;
-        },
-        info: function($mid) {
-          var def = $q.defer();
-          $http({
-            method: 'GET',
-            url: api.movie.getMovieInfo($mid)
-          }).then(function successCallback(response) {
-            def.resolve(response.data);
-          }, function errorCallback(response) {
-            def.reject(response);
-          });
-          return def.promise;
-        }
-
-      }
-    }])
-   .factory("MovieData", ['$q', function($q){
-     function movieCompareData() {
-       var self = this;
-
-       self.selectedMovies =[];
-       self.selectedMoviesInfo = [];
-       self.comparedMoviesInfo = [];
-
-       self.addSeleectedMovie = function() {}
-       self.compareMoviesList = function () {}
-       self.compareTwoMovies = function(movieA, movieB) {}
-       self.compareAllMovies = function() {}
-     }
-
-     return new movieCompareData();
-   }]);
-
-
-})();
-
-(function(){
-  'use strict';
-
-  //angular.module('services', ['ngMaterial']);
-
-})();
-
-(function(){
   'use strict';
 
   // Prepare the 'users' module for subsequent registration of controllers and delegates
@@ -299,5 +137,217 @@
       }
 
     }]);
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('castCtrl', []).controller('CastController', function($scope){
+      var vm = this;
+      vm.title = "Cast";
+
+    });
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('homeCtrl', []).controller('HomeController', function($scope){
+      var vm = this;
+      vm.title = "Home";
+      vm.sections = [
+           //{order:'0', title:'Special', controller:'SpecialController', type:'special'}
+           {order:'3', title:'Actors/Actresses', controller:'CastController', state:'cast', type:'cast-tab'},
+           {order:'1', title:'Movie', controller:'MovieController', state:'movies', type:'movie-tab' },
+           {order:'2', title:'Tv Shows', controller:'TvController', state:'tv', type:'tv-tab' }
+       ];
+    });
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('movieCtrl', ['ui.bootstrap']).controller('MovieController', ['movieServices','MovieData', function(movieServices, MovieData){
+      var vm = this;
+      vm.title = "Movie";
+
+      vm.resultsLimit = 8;
+      vm.selected = undefined;
+      vm.selectedMovie = undefined;
+      vm.showSelected = false;
+      vm.selectedCredit = undefined;
+      vm.showSelectedCredit = false;
+
+
+      vm.movieSearch = function(query) {
+        return movieServices.names(query).then(function (results) {
+          var moviedata = results.results;
+          return (moviedata.length > vm.resultsLimit ? moviedata.slice(0, vm.resultsLimit) : moviedata);  ;
+        }, function (error) {
+          console.log("ERROR NO RESULTS");
+        });
+      }
+
+      vm.getMovieInfo = function(movieid) {
+        return movieServices.info(movieid).then(function (results) {
+          return results;
+        }, function (error) {
+          console.log("ERROR NO MOVIE INFO");
+        });
+      }
+
+      vm.getMovieCredits = function(movieid) {
+        return movieServices.credits(movieid).then(function (results) {
+          return results;
+        }, function (error) {
+          console.log("ERROR NO MOVIE CREDITS");
+        });
+      }
+
+      vm.viewMovieInfo = function(item) {
+        vm.showSelected = false;
+        var movieid = item.id;
+        if((vm.selectedMovie == undefined) || (movieid != vm.selectedMovie.id)) {
+          vm.showSelectedCredit = false;
+          vm.selectedCredit = undefined;
+          vm.getMovieInfo(movieid).then(function(retResults) {
+            vm.showSelected = true;
+            vm.selectedMovie = retResults;
+          });
+        }
+        else {
+          vm.showSelected = true;
+        }
+      }
+
+      vm.viewMovieCredits = function(movieid) {
+        if(vm.showSelectedCredit == false){
+          if(vm.selectedCredit == undefined) {
+            vm.getMovieCredits(movieid).then(function(retResults) {
+              vm.selectedCredit = retResults.cast;
+              vm.showSelectedCredit = true;
+              console.log("Got Results");
+            });
+          }
+          else {
+            vm.showSelectedCredit = true;
+            console.log("Already Had Results");
+          }
+        }
+        else {
+          vm.showSelectedCredit = false;
+        }
+
+      }
+
+    }]);
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('specialCtrl', ['ui.bootstrap']).controller('SpecialController', [ 'movieServices','MovieData', function(movieServices, MovieData){
+      var vm = this;
+      vm.title = "Special";
+      vm.resultsLimit = 8;
+      vm.selected = undefined;
+
+      vm.movieSearch = function(query) {
+        return movieServices.names(query).then(function (results) {
+          var moviedata = results.results;
+          return (moviedata.length > vm.resultsLimit ? moviedata.slice(0, vm.resultsLimit) : moviedata);  ;
+        }, function (error) {
+          console.log("ERROR NO RESULTS");
+        });
+      }
+
+    }]);
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('tvCtrl', []).controller('TvController', function($scope){
+      var vm = this;
+      vm.title = "Tv";
+
+    });
+
+})();
+
+(function(){
+   "use strict";
+
+   angular.module('services', [])
+    .service('movieServices', ['$q', '$http','api', function MovieService($q, $http, api) {
+      return {
+        names: function($str){
+          var def = $q.defer();
+          $http({
+            method: 'GET',
+            url: api.movie.searchname($str)
+          }).then(function successCallback(response) {
+            def.resolve(response.data);
+          }, function errorCallback(response) {
+            def.reject(response);
+          });
+          return def.promise;
+        },
+        credits: function($mid){
+          var def = $q.defer();
+          $http({
+            method: 'GET',
+            url: api.movie.getMovieCredits($mid)
+          }).then(function successCallback(response) {
+            def.resolve(response.data);
+          }, function errorCallback(response) {
+            def.reject(response);
+          });
+          return def.promise;
+        },
+        info: function($mid) {
+          var def = $q.defer();
+          $http({
+            method: 'GET',
+            url: api.movie.getMovieInfo($mid)
+          }).then(function successCallback(response) {
+            def.resolve(response.data);
+          }, function errorCallback(response) {
+            def.reject(response);
+          });
+          return def.promise;
+        }
+
+      }
+    }])
+   .factory("MovieData", ['$q', function($q){
+     function movieCompareData() {
+       var self = this;
+
+       self.selectedMovies =[];
+       self.selectedMoviesInfo = [];
+       self.comparedMoviesInfo = [];
+
+       self.addSeleectedMovie = function() {}
+       self.compareMoviesList = function () {}
+       self.compareTwoMovies = function(movieA, movieB) {}
+       self.compareAllMovies = function() {}
+     }
+
+     return new movieCompareData();
+   }]);
+
+
+})();
+
+(function(){
+  'use strict';
+
+  //angular.module('services', []);
 
 })();
