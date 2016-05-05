c8b2b6ad192deda6b3319a841fab4d9627b5f26a
Kris Redding <kredding@inovas.net>
2016-05-05 14:05:07 -0400

    Begin compar functions

--- app/js/controllers/MovieCtrl.js
+++ app/js/controllers/MovieCtrl.js
@@ -1,7 +1,7 @@
 (function(){
    "use strict";
 
-    angular.module('movieCtrl', ['ui.bootstrap']).controller('MovieController', ['movieServices','MovieData', '$filter', function(movieServices, MovieData, $filter){
+    angular.module('movieCtrl', ['ui.bootstrap']).controller('MovieController', ['movieServices','MovieData', '$filter', '$q', function(movieServices, MovieData, $filter, $q){
       var vm = this;
       vm.title = "Movie";
 
@@ -17,9 +17,13 @@
       vm.showCompare = false;
       vm.selectedObjects = [];
       vm.slider = [false, false, false];
+      vm.compared = [];
 
       vm.movieSearch = function(query) {
-        return movieServices.names(query).then(function (results) {
+        var cleanString = query;
+        cleanString = cleanString.replace("&", "and");
+
+        return movieServices.names(cleanString).then(function (results) {
           var moviedata = results.results;
           return (moviedata.length > vm.resultsLimit ? moviedata.slice(0, vm.resultsLimit) : moviedata);  ;
         }, function (error) {
@@ -120,6 +124,94 @@
         var movieIndex = vm.getMovieIndex(movie);
         return vm.slider[movieIndex];
       }
+
+      /*Compare Movies*/
+      vm.compareSelectedMovies = function() {
+        console.log("-- COMPARE --");
+
+        vm.getCompareCasts().then(function (data){
+          console.log("Cast List: ");
+          console.log(data);
+          vm.compared = vm.compareMovieCasts(data);
+        });
+
+        //vm.compared = vm.compareMovieCasts(castList);
+        //console.log("Finished: ");
+        //console.log(vm.compared);
+
+      }
+
+      /*Get movie cast for each compared movie*/
+      vm.getCompareCasts = function() {
+
+        var compareCasts = [];
+        var deferred = $q.defer();
+
+        for(var i =0; i < vm.selectedObjects.length; i++) {
+          var movieCast = vm.getMovieCredits(vm.selectedObjects[i].id);
+          compareCasts.push({movieinfo: vm.selectedObjects[i], cast: movieCast });
+        }
+        deferred.resolve(compareCasts)
+        return deferred.promise;
+      }
+      /*Compare casts for each movie and return all cast members that appear in movies together*/
+      vm.compareMovieCasts = function(castList) {
+        console.log("COMPARE LIST");
+        console.log(castList);
+
+        var compareResults = [];
+        // compare one to one
+        if(castList.length > 1){
+          for(var i=0; i < castList.length; i++) {
+            for(var j=i+1; j < castList.length; j++) {
+              compareResults.push({movie1: castList[i].movieinfo, movie2:castList[j].movieinfo, matchedCast:vm.compareMovies(castList[i].cast, castList[j].cast, null) });
+            }
+          }
+        }
+        // compare all 3 items
+        if(castList.length == 3){
+          compareResults.push({movie1: castList[0].movieinfo, movie2:castList[1].movieinfo, movie3:castList[2].movieinfo, matchedCast:vm.compareMovies(castList[0], castList[1], castList[2]) });
+        }
+        console.log("COMPARE RESULTS");
+        console.log(compareResults);
+        return compareResults;
+      }
+
+      /*Compare movies and return cast in each movie*/
+      vm.compareMovies = function(movieA, movieB, movieC) {
+        console.log("movieA: " + movieA.length);
+        console.log(movieA);
+        console.log("movieB: " + movieB.length);
+        console.log(movieB);
+        console.log("movieC");
+        console.log(movieC);
+
+        var comparedCasts = [];
+        // Only compare 2
+        if(movieC == null) {
+          for(var i=0; i < movieA.length; i++) {
+            for(var j=0; j < movieB.length; j++) {
+              if(movieA[i].id == movieB[j].id){
+                comparedCasts.push(movieA[i]);
+              }
+            }
+          }
+        }
+        else {
+          for(var i=0; i < movieA.length; i++) {
+            for(var j=0; j < movieB.length; j++) {
+              for(var k=0; k < movieC.length; k++) {
+                if((movieA[i].id == movieB[j].id) && (movieB[i].id == movieC[k].id)) {
+                  comparedCasts.push(movieA[i]);
+                }
+              }
+            }
+          }
+        }
+
+        return comparedCasts;
+      }
+
 
     }]);
 
