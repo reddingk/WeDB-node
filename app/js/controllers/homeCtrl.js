22df07482de8e5540f03eddd331345c87ef8820c
Kris Redding <kredding@autoscribe.com>
2016-10-26 17:18:08 -0400

    update home design

--- app/js/controllers/homeCtrl.js
+++ app/js/controllers/homeCtrl.js
@@ -5,6 +5,8 @@
       var vm = this;
       vm.title = "Home";
 
+      vm.latestBlog = {"img1":"http://www.impawards.com/2016/posters/tarzan_ver3_xlg.jpg", "img2":"http://questionablefilmreview.files.wordpress.com/2013/07/7736093674_2e8414a35c_o.jpg", "text":"Whose headed to purchase @legendoftarzan available on blu-Ray and DVD TODAY!!! We wanted to find a #wedbconnection and we found one with one of our all time favorite actors @samuelljackson and co-star #ChristophWaltz This will be third time the pair have joined eachother for a big screen production! First in 2009 when Sam narrated for the film #IngloriousBasterds starring #BradPitt then again when the both graced the screen in the unique #QuentinTarantino film #DjangoUnchained starring the multitalented @iamjamiefoxx to their most recent action film to hit theaters @legendoftarzan a definite must see starring another one of our favorite actresses @margotrobbie as Jane and #AlexanderSkarsgard as Tarzan! With his incredible range and amazingly diverse talents we can't wait to see what @samuelljackson will do next! #SamuelLJackson #ChristophWaltz #MargotRobbie #AlexanderSkarsgard #JamieFoxx #BradPitt #IngloriousBasterds #DjangoUnchained #LegendOfTarzan #wedbconnection"}
+
       vm.headerTemplate = "views/templates/_header.html";
       vm.homeImg = "imgs/siteart/Home6.jpg";
 
