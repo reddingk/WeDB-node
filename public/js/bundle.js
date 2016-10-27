9ef20ef08e2fcb1bcc33c8dda97214d103ac08c4
Kris Redding <kris.redding3@gmail.com>
2016-10-26 22:47:47 -0400

    update design

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -45,22 +45,6 @@
 })();
 
 (function(){
-   "use strict";
-
-    angular.module('directives').directive('backImg', ['$window', function($window) {
-      return {
-        restrict: 'EA',
-        link: function ($scope, element, attrs) {
-          var url = attrs.backImg;
-          element.css({'background-image': 'url(' + url +')'});
-        }
-      }
-
-    }]);
-
-})();
-
-(function(){
   'use strict';
 
   angular
@@ -121,5 +105,83 @@
       //$locationProvider.html5Mode(true);
     }]);
 
+
+})();
+
+(function(){
+  'use strict';
+
+  angular
+    .module('config')
+    .factory('api', function(){
+      var baseurl = "https://api.themoviedb.org/3/";
+      var apikey = "8af02f398b3ff990bab4f71c247c640a";
+
+      return {
+        any: {
+          all: function(query){
+            return baseurl + "search/multi?api_key="+apikey+"&query="+query;
+          }
+        },
+         movie: {
+             searchname: function(query){
+                 return baseurl + "search/movie?api_key="+apikey+"&query="+query;
+             },
+             searchName_Page: function(query, page){
+                 return baseurl + "search/movie?api_key="+apikey+"&page="+ page +"&query="+query;
+             },
+             getMovieCredits: function(id) {
+                 return baseurl + "movie/"+id+"/credits?api_key="+apikey;
+             },
+             getMovieInfo: function(id) {
+                 return baseurl + "movie/"+id+"?api_key="+apikey;
+             }
+         },
+         cast: {
+             searchname: function(query) {
+                 return  baseurl + "search/person?api_key="+apikey+"&query="+query;
+             },
+             searchName_Page: function(query, page){
+                 return baseurl + "search/person?api_key="+apikey+"&page="+ page +"&query="+query;
+             },
+             getCastCredits: function(id) {
+                 return baseurl + "person/"+id+"/movie_credits?api_key="+apikey;
+             },
+             getCastInfo: function(id) {
+                 return baseurl + "person/"+id+"?api_key="+apikey;
+             }
+         },
+         tv: {
+             searchname: function(query){
+                 return baseurl + "search/tv?api_key="+apikey+"&query="+query;
+             },
+             searchName_Page: function(query, page){
+                 return baseurl + "search/tv?api_key="+apikey+"&page="+ page +"&query="+query;
+             },
+             getTvCredits: function(id) {
+                 return baseurl + "tv/"+id+"/credits?api_key="+apikey;
+             },
+             getTvInfo: function(id) {
+                 return baseurl + "tv/"+id+"?api_key="+apikey;
+             }
+         }
+      }
+    });
+
+})();
+
+(function(){
+   "use strict";
+
+    angular.module('directives').directive('backImg', ['$window', function($window) {
+      return {
+        restrict: 'EA',
+        link: function ($scope, element, attrs) {
+          var url = attrs.backImg;
+          element.css({'background-image': 'url(' + url +')'});
+        }
+      }
+
+    }]);
 
 })();
